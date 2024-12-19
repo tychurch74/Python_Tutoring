c5011b21db0a3e2c50d498d93229786086df07bf
Tyler Church <tychurch74@gmail.com>
2024-12-19 14:08:34 -0500

    ifs and lists

---
+++ Basics/3-If Statements.py
@@ -0,0 +1,8 @@
+
+x = input("type any number: ")
+
+if x == 1:
+    print("User typed 1")
+
+else:
+    print("User did not type 1")
---
+++ Basics/4-Lists.py
@@ -0,0 +1,3 @@
+first_list = [2, 5, 9, 16]
+
+second_list = ["apple", "orange", "strawberry"]
